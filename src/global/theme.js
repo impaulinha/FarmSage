c0b4023706db1513a290bdb7878d73a8bb7c8f6f
impaulinha <anaaguiar20016@gmail.com>
2023-09-01 20:44:59 -0300

    Config. inicial

---
+++ src/global/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+    color:{
+        white: 'FFFFFF',
+        black: '000000',
+        paleGreen: '73C878',
+        green: '00AD2C',
+        cyanGreen: '00BB89',
+        grapeGreen: 'B3F6B6'
+    }
+}
